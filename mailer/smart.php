a9c81ee3243df10f19749d9c27d1b5ee888be3c0
Taleh <talehqarayev@mail.ru>
2020-07-02 17:11:54 +0300

    bugs

--- mailer/smart.php
+++ mailer/smart.php
@@ -18,7 +18,7 @@ $mail->Password = 'Qarayev1996';                           // Наш парол�
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('Данные о сайте', 'Pulse');   // От кого письмо 
+$mail->setFrom('shukran.garayev96@gmail.com', 'Pulse');   // От кого письмо 
 $mail->addAddress('talehqarayev@mail.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
